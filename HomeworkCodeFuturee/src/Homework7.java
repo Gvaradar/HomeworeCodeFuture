404d870a25383416ddbf7073f4e1f14e51fe8211
Djon <lioni767@yandex.ru>
2023-10-31 21:40:47 +0300

    do homework 7

---
+++ HomeworkCodeFuturee/src/Homework7.java
@@ -0,0 +1,13 @@
+public class Homework7 {
+    public static void main(String[] args) {
+        rectArea(5);
+        rectArea(4,1);
+
+    }
+    public static void rectArea(double side){
+        double s = side*side;
+    }
+    public static void rectArea(double width,double height){
+        double sRectangle = width*height;
+    }
+}
